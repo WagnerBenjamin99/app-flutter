805d86e6e411c017aceb795236689641ad47f51e
WagnerBenjamin <benjaminwagner@outlook.com>
2023-12-14 16:02:14 -0300

    registro individual

--- lib/screens/item_screen.dart
+++ lib/screens/item_screen.dart
@@ -3,21 +3,21 @@ import 'package:flutter_repo_guide/screens/itemFilter_screen.dart';
 import 'package:flutter_repo_guide/screens/itemList_screen.dart';
 import 'package:flutter_repo_guide/widgets/customDrawe.dart';
 import 'package:flutter_repo_guide/widgets/myCard_Song.dart';
+import 'package:flutter_repo_guide/widgets/myCard_artist.dart';
 
 class ItemScreen extends StatelessWidget {
-  final MyCard_Song myCard = MyCard_Song(
-    titulo: 'Amor amarillo',
-    artista: 'Gustavo Cerati',
-    duracion: '2:30',
-    imagenUrl:
-        'https://images-na.ssl-images-amazon.com/images/I/71P8kbXVHJL._SL1500_.jpg',
+  final MyCard_Artist myCard = MyCard_Artist(
+    nombre: 'Gustavo Cerati',
+    generos: ['rock', 'pop'],
+    popularidad: '89',
+    imagenUrl: 'https://fotos.perfil.com/2022/08/11/gustavo-cerati-1400969.jpg',
   );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(myCard.titulo),
+        title: Text(myCard.nombre),
         backgroundColor: Color.fromRGBO(33, 134, 18, 0.992),
       ),
       drawer: CustomDrawer(),
---
+++ lib/widgets/myCard_artist.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class MyCard_Artist extends StatelessWidget {
+  final String nombre;
+  final String popularidad;
+  final List<String> generos;
+  final String imagenUrl;
+
+  MyCard_Artist({
+    required this.nombre,
+    required this.popularidad,
+    required this.generos,
+    required this.imagenUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    String generosConcat = generos.join(' - ');
+
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxWidth: 350,
+        maxHeight: 400,
+      ),
+      child: Card(
+        elevation: 3,
+        margin: EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(color: Colors.grey),
+        ),
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: 200,
+                maxHeight: 250,
+              ),
+              child: Image(
+                image: NetworkImage(imagenUrl),
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.grey,
+                    child: Center(
+                      child: Icon(Icons.error),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      nombre,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Popularidad: $popularidad',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Géneros: $generosConcat',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
+                          fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
