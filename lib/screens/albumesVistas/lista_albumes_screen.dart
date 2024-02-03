4264ae1b23d2242b1363cbd2c26e7bdf5abace3e
AntonioDelGreco <antoniodelgreco21@gmail.com>
2024-02-02 22:31:12 -0300

    commit de stash

--- lib/screens/albumesVistas/lista_albumes_screen.dart
+++ lib/screens/albumesVistas/lista_albumes_screen.dart
@@ -5,7 +5,46 @@ import 'package:flutter_repo_guide/widgets/custom_card.dart';
 
 class ListaAlbumes extends StatelessWidget {
   final int cantidad;
-  const ListaAlbumes({super.key, required this.cantidad});
+
+  const ListaAlbumes({Key? key, required this.cantidad}) : super(key: key);
+
+  @override
+  _ListaAlbumesState createState() => _ListaAlbumesState();
+}
+
+class _ListaAlbumesState extends State<ListaAlbumes> {
+  List<Map<String, dynamic>> albums = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAlbumsAzarData();
+  }
+
+  Future<void> _fetchAlbumsAzarData() async {
+    try {
+      final response = await http.get(Uri.parse('http://localhost:8000/api/v1/albums-actuales'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = json.decode(response.body);
+
+        setState(() {
+          albums = data.map((album) {
+            return {
+              'nombre': album["name"],
+              'cantidadCanciones': album['total_tracks'],
+              'albumURL': album['images'][0]['url'],
+            };
+          }).toList();
+          albums.shuffle();
+        });
+      } else {
+        throw Exception('Fallo en leer los datos de la API.');
+      }
+    } catch (error) {
+      throw Exception('Fallo en leer los datos de la API.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
